4e090b16d620599114dfc6d10938f1018564681d
ngocdydznek <168576578+ngocdydznek@users.noreply.github.com>
2024-09-29 03:01:55 +0700

    keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -2,13 +2,13 @@ const express = require('express');
 const server = express();
 
 server.all('/', (req, res) => {
-    res.send('Bot is running!');
+  res.send('Bot đang hoạt động!');
 });
 
 function keepAlive() {
-    server.listen(3000, () => {
-        console.log('Server is ready.');
-    });
+  server.listen(3000, () => {
+    console.log("Server đã hoạt động!");
+  });
 }
 
 module.exports = keepAlive;
